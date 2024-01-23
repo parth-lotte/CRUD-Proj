fadd923088350c1868b51d6105212d06e300921c
parth-lotte <parth33998@gmail.com>
2024-01-24 01:08:56 +0530

    Creating a register page

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 
@@ -8,3 +9,31 @@ def home(request):
     # return HttpResponse('Hello Parth Lotte !')
     
     return render(request, 'webapp/index.html')
+
+
+
+# -- Register 
+
+def register(request):
+    
+    form= CreateUserForm()
+    
+    if request.method=="POST":
+        
+        form = CreateUserForm(request.POST)
+        
+        if form.is_valid():
+            
+            form.save()
+            
+            # return redirect()
+            
+
+    context= {'form':form}
+    
+    return render(request, 'webapp/register.html', context=context)
+
+
+
+
+
