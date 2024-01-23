993774d4cfc192563773c00604c15ac37ad919e8
parth-lotte <parth33998@gmail.com>
2024-01-24 01:33:13 +0530

    register page

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -3,5 +3,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home, name="")
+    path('',views.home, name=""),
+    path('register', views.register, name='register'),
 ]
