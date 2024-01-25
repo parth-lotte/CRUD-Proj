f65b764f7ab262614bf12faebab33796ad521b41
parth-lotte <parth33998@gmail.com>
2024-01-25 19:49:32 +0530

    Created Models

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
 
-# Create your views here.
+# for Login page and authentication 
+from django.contrib.auth.models import auth 
+from django.contrib.auth import authenticate
 
 # from django.http import HttpResponse 
+
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     # return HttpResponse('Hello Parth Lotte !')
@@ -26,7 +30,7 @@ def register(request):
             
             form.save()
             
-            # return redirect()
+            return redirect('my-login') # redirect to the login page 
             
 
     context= {'form':form}
@@ -35,5 +39,44 @@ def register(request):
 
 
 
+# Login - Page 
+
+def my_login(request):
+    
+    form= LoginForm()
+    
+    if request.method=="POST":
+        form= LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+            
+            username= request.POST.get('username')
+            password= request.POST.get('password')
+            
+            user= authenticate(request, username=username , password=password)
+            
+            if user is not None:
+                
+                auth.login(request, user)
+                
+                return redirect("dashboard")
+                 
+    context= {'form2':form}
+
+    return render(request, 'webapp/my-login.html', context=context)
 
 
+#  User Log Out 
+
+def user_logout(request):
+    auth.logout(request)
+    
+    return redirect("my-login")
+
+# DashBoard
+
+# we will be giving the route name to the following 
+
+@login_required(login_url='my-login')
+def dashboard(request):
+    return render(request, 'webapp/dashboard.html')
