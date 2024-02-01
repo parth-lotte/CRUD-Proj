3899b14333de3bfcc8081491e55c2bd7b14d1c87
parth-lotte <parth33998@gmail.com>
2024-02-01 17:55:02 +0530

    ec2  deploy

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -16,10 +16,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_kuzywzq3)3fa5+d#qq)vnr2%y8*$^v6@y!aqa$x2d&r+%m@cj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # changed this to false ******RC*******
 
-ALLOWED_HOSTS = [*]
+# ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['3.27.191.155', 'CRUDX']
+
 # replaceddddd hosts 
 
 
